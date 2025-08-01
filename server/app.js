f11a0f62d264e251ac1334dc7bab409b5beba2d9
James Morrissey <jammim19@gmail.com>
2025-08-01 17:15:32 +0100

    Added the code for uploading the pdf to the server

--- server/app.js
+++ server/app.js
@@ -1,10 +1,39 @@
 import express from "express";
 import dotenv from "dotenv";
 import multer from "multer";
+import path from "path";
 
 dotenv.config();
 
-const upload = multer({ dest: "uploads/", preservePath: true });
+// Set up storage options
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, `(${Date.now()})-${file.originalname}`);
+  },
+});
+
+// Initialize multer
+const upload = multer({
+  storage: storage,
+  limits: { fileSize: 5 * 1024 * 1024, files: 1 },
+  fileFilter: (req, file, cb) => {
+    const filetypes = /pdf/;
+    const extname = filetypes.test(
+      path.extname(file.originalname).toLowerCase()
+    );
+    const mimetype = filetypes.test(file.mimetype);
+
+    if (extname && mimetype) {
+      cb(null, true);
+    } else {
+      cb(new Error("Only pdf's are allowed"));
+    }
+  },
+});
+
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -21,9 +50,14 @@ app.post(
   "/upload_pdf",
   upload.single("pdf-to-sign"),
   (request, response, next) => {
-    response.json({ msg: "File uploaded" });
-    response.statusCode = 200;
-    console.log(request.file);
+    if (!request.file.mimetype == "application/pdf") {
+      response.sendStatus(406);
+      response.end();
+    } else {
+      response.statusCode = 200;
+      console.log(request.file);
+      response.end();
+    }
   }
 );
 
