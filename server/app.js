e0592d9599536d63e1d8eea295044bcfc6ef35a2
James Morrissey <jammim19@gmail.com>
2025-08-01 09:51:33 +0100

    Added Multer

--- server/app.js
+++ server/app.js
@@ -1,10 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
+import multer from "multer";
 
 dotenv.config();
 
+const upload = multer({ dest: "uploads/", preservePath: true });
 const app = express();
-
 const port = process.env.PORT || 3000;
 
 app.get("/", (request, response) => {
@@ -13,11 +14,18 @@ app.get("/", (request, response) => {
 
 app.get("/signed_pdf", (request, response) => {
   response.send("signed pdf GET response");
+  // response.download()
 });
 
-app.post("/upload_pdf", (request, response) => {
-  response.send("Pdf uploaded");
-});
+app.post(
+  "/upload_pdf",
+  upload.single("pdf-to-sign"),
+  (request, response, next) => {
+    response.json({ msg: "File uploaded" });
+    response.statusCode = 200;
+    console.log(request.file);
+  }
+);
 
 app.listen(port, () => {
   console.log(`Listening on port : ${port}`);
