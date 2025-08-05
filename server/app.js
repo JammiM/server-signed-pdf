708aa8df7c397a78b00c6556bd20c26c7b6360c9
James Morrissey <jammim19@gmail.com>
2025-08-05 14:49:26 +0100

    Set up the outline of the middleware for signing the pdf

--- server/app.js
+++ server/app.js
@@ -2,9 +2,12 @@ import express from "express";
 import dotenv from "dotenv";
 import multer from "multer";
 import path from "path";
+import { fileURLToPath } from "url";
 
 dotenv.config();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 // Set up storage options
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
@@ -37,9 +40,34 @@ const upload = multer({
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (request, response) => {
-  response.send("Set up ready");
-});
+const signPdf = function (request, response, next) {
+  // console.log(request.file.path);
+
+  //load the file (buffer)
+
+  //establish file path
+
+  //edit the file
+
+  // write to the file
+
+  // return html page with download link
+
+  next();
+};
+
+const createPdfDownloadLink = function (request, response, next) {
+  const filePath = path.join(__dirname, request.file.path);
+
+  console.log(filePath);
+
+  const responseText = `<a download href=${filePath} target="_blank">Download signe pdf</a>`;
+  response.send(responseText);
+
+  next();
+};
+
+const signPdfHandler = [signPdf, createPdfDownloadLink];
 
 app.get("/signed_pdf", (request, response) => {
   response.send("signed pdf GET response");
@@ -55,10 +83,11 @@ app.post(
       response.end();
     } else {
       response.statusCode = 200;
-      console.log(request.file);
+      next();
       response.end();
     }
-  }
+  },
+  signPdfHandler
 );
 
 app.listen(port, () => {
