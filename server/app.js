e80ee8a0574edc73c2fa4384997cdbed04b55b98
James Morrissey <jammim19@gmail.com>
2025-08-06 21:20:24 +0100

    Added a global error handler

--- server/app.js
+++ server/app.js
@@ -1,13 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
-
+import { errorHandler } from "./middleware/errorHandler.js";
 import { router } from "./routes/pdfRoutes.js";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
-
+app.use(errorHandler);
 app.use("/api", router);
 
 app.listen(port, () => {
