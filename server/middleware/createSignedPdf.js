30a7acdf2820488757f8dd7682e02e70935c1153
James Morrissey <jammim19@gmail.com>
2025-08-06 21:07:20 +0100

    Added to ability to create an signed pdf on the server

--- server/middleware/createSignedPdf.js
+++ server/middleware/createSignedPdf.js
@@ -1,6 +1,51 @@
-const createSignedPdf = function (req, res, next) {
-  req.requestTime = Date.now();
+import { PDFDocument, StandardFonts, rgb } from "pdf-lib";
+import fs from "node:fs";
+
+async function createPdf() {
+  const pdfDoc = await PDFDocument.create();
+  const timesRomanFont = await pdfDoc.embedFont(StandardFonts.TimesRoman);
+
+  const page = pdfDoc.addPage();
+  const { width, height } = page.getSize();
+  const fontSize = 30;
+  page.drawText("Creating PDFs in JavaScript is awesome!", {
+    x: width + 50,
+    y: height - 4 * fontSize,
+    size: fontSize,
+    font: timesRomanFont,
+    color: rgb(0, 0.53, 0.71),
+  });
+
+  const pdfBytes = await pdfDoc.save();
+  const pdfBuffer = Buffer.from(pdfBytes.buffer, "binary");
+
+  return pdfBuffer;
+}
+
+const createSignedPdf = function (req, response, next) {
+  createPdf()
+    .then(function (pdfBuffer) {
+      try {
+        process.chdir("signed_pdf_file");
+      } catch (err) {
+        console.error(err.message);
+      }
+      writeFromBuffer(pdfBuffer);
+    })
+    .catch(function (err) {
+      console.error(err.message);
+    });
   next();
+};
+
+const writeFromBuffer = function (pdfBuffer) {
+  fs.writeFile(`${Date.now()}-Signed.pdf`, pdfBuffer, (err) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log("File written & Fire clean up / unlink");
+    }
+  });
 };
 
 export { createSignedPdf };
