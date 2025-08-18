6ada712fa197bb410ccf470fc7b71bf7a7e32b49
James Morrissey <jammim19@gmail.com>
2025-08-18 15:07:08 +0100

    Assigned the root page, to the form controller

--- server/routes/pdfRoutes.js
+++ server/routes/pdfRoutes.js
@@ -1,8 +1,11 @@
 import express from "express";
 import { upload } from "../multer-config.js";
 import { pdfUploadCtrl } from "../controllers/pdfUploadController.js";
+import { pdfFormCtrl } from "../controllers/pdfFormController.js";
 
 const router = express.Router();
+
+router.get("/", pdfFormCtrl);
 
 router.post("/upload_pdf", upload.single("pdf-to-sign"), pdfUploadCtrl);
 
